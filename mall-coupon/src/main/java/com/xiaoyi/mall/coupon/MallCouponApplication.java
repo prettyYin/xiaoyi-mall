803782fbe53f9ca513413ac5b42ca018d97dca88
yzj <yinzejin>
2023-12-16 01:04:10 +0800

    nacos服务发现注册

--- mall-coupon/src/main/java/com/xiaoyi/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/xiaoyi/mall/coupon/MallCouponApplication.java
@@ -3,7 +3,9 @@ package com.xiaoyi.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
 @MapperScan("com.xiaoyi.mall.coupon.dao")
 @SpringBootApplication
 public class MallCouponApplication {
