09bd0c72bc32e40b1bc6903efd797709bf4a2206
yzj <yinzejin>
2023-12-17 14:09:38 +0800

    添加nacos配置中心

--- mall-coupon/src/main/java/com/xiaoyi/mall/coupon/controller/HomeAdvController.java
+++ mall-coupon/src/main/java/com/xiaoyi/mall/coupon/controller/HomeAdvController.java
@@ -4,11 +4,9 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 import com.xiaoyi.mall.coupon.entity.HomeAdvEntity;
 import com.xiaoyi.mall.coupon.service.HomeAdvService;
@@ -24,11 +22,23 @@ import com.xiaoyi.mall.common.utils.R;
  * @email 2544212327@qq.com
  * @date 2023-12-13 22:44:38
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/homeadv")
 public class HomeAdvController {
     @Autowired
     private HomeAdvService homeAdvService;
+
+    // 测试nacos config是否生效
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private Integer age;
+    @GetMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
+
 
     /**
      * 列表
