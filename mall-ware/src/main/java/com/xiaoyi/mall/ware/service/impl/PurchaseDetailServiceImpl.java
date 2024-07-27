b3ccf7d1155044c2344d54fb68463c447a08486f
Chandler <2544212327@qq.com>
2024-07-28 04:14:20 +0800

    fix(ware):采购需求列表查询接口=》新增条件查询

--- mall-ware/src/main/java/com/xiaoyi/mall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ mall-ware/src/main/java/com/xiaoyi/mall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.xiaoyi.mall.ware.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,35 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageInfo queryPage(Map<String, Object> params) {
+        /**
+         * status: 0,//状态
+         *    wareId: 1,//仓库id
+         */
+
+        QueryWrapper<PurchaseDetailEntity> queryWrapper = new QueryWrapper<PurchaseDetailEntity>();
+
+        String key = (String) params.get("key");
+        if(!StrUtil.isEmpty(key)){
+            //purchase_id  sku_id
+            queryWrapper.and(w->{
+                w.eq("purchase_id",key).or().eq("sku_id",key);
+            });
+        }
+
+        String status = (String) params.get("status");
+        if(!StrUtil.isEmpty(status)){
+            //purchase_id  sku_id
+            queryWrapper.eq("status",status);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StrUtil.isEmpty(wareId)){
+            //purchase_id  sku_id
+            queryWrapper.eq("ware_id",wareId);
+        }
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                queryWrapper
         );
 
         return new PageInfo(page);
