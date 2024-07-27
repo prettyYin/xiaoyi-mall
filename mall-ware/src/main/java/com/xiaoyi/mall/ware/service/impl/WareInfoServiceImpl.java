65838511c93ccc8b6c7f9ff4fa225e90ac49c2bd
Chandler <2544212327@qq.com>
2024-07-28 04:14:33 +0800

    fix(ware):采购需求列表查询接口=》新增条件查询

--- mall-ware/src/main/java/com/xiaoyi/mall/ware/service/impl/WareInfoServiceImpl.java
+++ mall-ware/src/main/java/com/xiaoyi/mall/ware/service/impl/WareInfoServiceImpl.java
@@ -2,17 +2,16 @@ package com.xiaoyi.mall.ware.service.impl;
 
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xiaoyi.mall.common.utils.PageInfo;
 import com.xiaoyi.mall.common.utils.Query;
-
 import com.xiaoyi.mall.ware.dao.WareInfoDao;
 import com.xiaoyi.mall.ware.entity.WareInfoEntity;
 import com.xiaoyi.mall.ware.service.WareInfoService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
